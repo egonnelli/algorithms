5bc0d19a6c142231521902921d54c7a3591eeb29
egonnelli <egonnelli@gmail.com>
2020-08-16 05:42:16 +0200

    func bubblesort

--- sorting_algorithms/01_bubblesort.py
+++ sorting_algorithms/01_bubblesort.py
@@ -2,6 +2,19 @@
 #Time complexity - O(N^2)
 
 def bubble_sort(array):
+	"""
+	This functions implements the bubble sort algorithm
+	"""
+	is_sorted = False
+	counter = 0
+	while not is_sorted
+		is_sorted = True
+		for i in range(len(array) - 1 - counter)
+			if array[i] > array[i+1]:
+				swap(i, i+1, array)
+				is_sorted = False
+		counter += 1
+	return array
 
 
 def swap(i,j,array):
