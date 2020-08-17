cb0a693d31a901a571b0cdfc1b83a6e8a1904a75
egonnelli <egonnelli@gmail.com>
2020-08-17 06:41:59 +0200

    helper func 1

--- sorting_algorithms/02_quick_sort.py
+++ sorting_algorithms/02_quick_sort.py
@@ -3,4 +3,15 @@
 def quick_sort(array):
 
 def quick_sort_helper(array, start_idx, end_idx):
+	
+	if start_idx >= end_idx:
+		return
+
+	pivot_idx = start_idx
+	left_idx = start_idx + 1
+	right_idx = end_idx
+
+	while right_idx >= left_idx:
+		if array[left_idx] > array[pivot_idx] and array[right_idx] < array[pivot_idx]:
+			swap(left_idx, right_idx, array)
 
