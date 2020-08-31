f6c0303020fe24d3a77776209ead49c6d1886d8a
egonnelli <egonnelli@gmail.com>
2020-08-31 18:37:32 +0200

    First exercise

---
+++ hackerrank/warm-up-challenges/01-solve-me-first.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int num1 = a;
+  int num2 = b;
+  int sum = num1 + num2;
+  return sum;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
