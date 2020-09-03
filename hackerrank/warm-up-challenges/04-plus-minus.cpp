395d6fadee9e353194cdafa8e7c62d97e73d6486
egonnelli <egonnelli@gmail.com>
2020-09-03 18:48:15 +0200

    Plus Minus

--- hackerrank/warm-up-challenges/04-plus-minus.cpp
+++ hackerrank/warm-up-challenges/04-plus-minus.cpp
@@ -12,5 +12,24 @@ void plusMinus(vector<int> arr) {
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
 }
