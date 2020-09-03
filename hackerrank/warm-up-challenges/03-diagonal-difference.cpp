8abec166aba60caa6e4eb979e2e463d0f1de7309
egonnelli <egonnelli@gmail.com>
2020-09-03 02:01:42 +0200

    Diagonal difference

--- hackerrank/warm-up-challenges/03-diagonal-difference.cpp
+++ hackerrank/warm-up-challenges/03-diagonal-difference.cpp
@@ -14,6 +14,25 @@ vector<string> split(const string &);
  */
 
 int diagonalDifference(vector<vector<int>> arr) {
+    
+    int sum1 = 0;
+    int sum2 = 0;
+
+for(int i = 0; i < arr.size(); i++){
+    for(int j = 0; j < arr.size(); j++){
+        if(i==j){
+            sum1 += arr[i][j];
+            }
+        if((i+j) == (arr.size() - 1) ){
+            sum2 += arr[i][j];
+        }
+
+    }
+
+}
+
+int diff = abs(sum1-sum2);
+return diff;
 
 }
 
