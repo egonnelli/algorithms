bc8beaf4157982731c62d774a06be66093e58198
egonnelli <egonnelli@gmail.com>
2020-09-13 23:56:15 +0200

    Array sum

---
+++ hackerrank/warm-up-challenges/10-array-sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+int simpleArraySum(vector<int> ar) {
+    /*
+     * Write your code here.
+     */
+
+}
+
+int main()
+{
+    
+}
