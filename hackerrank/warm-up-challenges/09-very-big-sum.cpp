3bf66372721af0149768f7f75ac9a95d0741f630
egonnelli <egonnelli@gmail.com>
2020-09-16 08:35:53 +0200

    Big sum

--- hackerrank/warm-up-challenges/09-very-big-sum.cpp
+++ hackerrank/warm-up-challenges/09-very-big-sum.cpp
@@ -7,8 +7,15 @@ vector<string> split_string(string);
 // Complete the aVeryBigSum function below.
 long aVeryBigSum(vector<long> ar) {
 
+long sum = 0;
+
+for(int i = 0; i < ar.size(); i++){
+    sum += ar[i];
+}
+return sum;
 
 }
+
 
 int main()
 {
